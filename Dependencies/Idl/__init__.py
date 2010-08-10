c461167fd5b76dcddd3cb9089526a0703f1e74b2
Kip Streithorst <kstreith@ball.com>
2010-08-10 20:13:36 +0000

    Moved trunk/1.1.0 into releases folder for archive purposes.

---
+++ Dependencies/Idl/__init__.py
@@ -0,0 +1,4 @@
+__doc__ = """ITT IDL Libraries."""
+package = {'url':"https://opticks.ballforge.net/files/documents/8/366/Idl-11Jan2010.zip",
+           'downloadLocation':'Idl-11Jan2010.zip',
+           'targetDirectory':'Idl'}
